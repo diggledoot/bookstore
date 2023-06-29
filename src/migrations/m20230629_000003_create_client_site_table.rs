5b91da8cd6ad680a00279a2eac678793317a2dbb
Darren Chan <dfchan@idsdatanet.com>
2023-06-29 21:00:38 +0800

    fleshed out client site

--- src/migrations/m20230629_000003_create_client_site_table.rs
+++ src/migrations/m20230629_000003_create_client_site_table.rs
@@ -21,6 +21,14 @@ impl MigrationTrait for Migration {
                             .auto_increment()
                             .primary_key(),
                     )
+                    .col(ColumnDef::new(ClientSite::IndexWellUrl).string())
+                    .col(ColumnDef::new(ClientSite::Username).string())
+                    .col(ColumnDef::new(ClientSite::Password).string())
+                    .col(ColumnDef::new(ClientSite::IsIndexed).boolean())
+                    .col(ColumnDef::new(ClientSite::IsIndexing).boolean())
+                    .col(ColumnDef::new(ClientSite::LastIndexedAt).date_time())
+                    .col(ColumnDef::new(ClientSite::TotalToIndex).integer())
+                    .col(ColumnDef::new(ClientSite::TotalIndexed).integer())
                     .to_owned(),
             )
             .await
@@ -36,4 +44,12 @@ impl MigrationTrait for Migration {
 pub enum ClientSite {
     Table,
     Id,
+    IndexWellUrl,
+    Username,
+    Password,
+    IsIndexed,
+    IsIndexing,
+    LastIndexedAt,
+    TotalToIndex,
+    TotalIndexed,
 }
