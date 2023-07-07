ea4e33e7c6ee966ff7bf0f7a199ff5b43bb5474d
Darren Chan <dfchan@idsdatanet.com>
2023-07-07 09:14:39 +0800

    using something im more familiar with

--- src/migrations/migrator.rs
+++ src/migrations/migrator.rs
@@ -1,11 +1,11 @@
 use sea_orm_migration::prelude::*;
 pub struct Migrator;
 
-use super::m20230629_000001_create_well_discovery_config_table;
-use super::m20230629_000002_create_well_discovery_table;
-use super::m20230629_000003_create_client_site_table;
-use super::m20230629_000004_add_fk_client_site_to_wdc;
-use super::m20230629_000005_add_fk_client_site_to_wd;
+use crate::migrations::m20230629_000001_create_well_discovery_config_table;
+use crate::migrations::m20230629_000002_create_well_discovery_table;
+use crate::migrations::m20230629_000003_create_client_site_table;
+use crate::migrations::m20230629_000004_add_fk_client_site_to_wdc;
+use crate::migrations::m20230629_000005_add_fk_client_site_to_wd;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
